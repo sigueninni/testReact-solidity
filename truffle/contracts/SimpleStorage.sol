de7949090af5f94ac2e43701f7127b690d49fffa
sigueninni <saad.igueninni@gmail.com>
2022-11-12 16:58:57 +0100

    first commit

---
+++ truffle/contracts/SimpleStorage.sol
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+contract SimpleStorage {
+  uint256 value;
+
+  event valueChanged(uint _val);
+
+  function read() public view returns (uint256) {
+    return value;
+  }
+
+  function write(uint256 newValue) public {
+    value = newValue;
+    emit valueChanged(newValue);
+  }
+}
